ff8f9673bdb15967510124ae1aa8123f2f3d537e
urcm <76536007+urcm@users.noreply.github.com>
2021-11-24 20:33:46 +0100

    Update change_file_names_in_folders.py

--- basic/change_file_names_in_folders.py
+++ basic/change_file_names_in_folders.py
@@ -1,1 +1,24 @@
-
+for root, subdirectories, files in os.walk(directory):
+    for (a, subdirectory) in enumerate(subdirectories, start=1):
+        path = os.path.join(root, subdirectory)
+        file_counter = 1
+        for filename in os.listdir(path):
+            if re.search(pattern, filename):
+                try:
+                    os.rename(os.path.join(path, filename), path + '/' + folder_name + str(a) + '_' + 'icon' + '.png')
+                    print(filename)
+                except OSError as e:
+                    if e.errno == errno.EEXIST:
+                        print('File not renamed.')
+                    else:
+                        raise
+            else:
+                print('no match')
+                try:
+                    os.rename(os.path.join(path, filename), path + '/' + folder_name + str(a) + '_' + str(file_counter) + '.png')
+                except OSError as e:
+                    if e.errno == errno.EEXIST:
+                        print('File not renamed.')
+                    else:
+                        raise
+            file_counter += 1
