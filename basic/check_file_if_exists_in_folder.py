f7f3538c848290e1451e2fe8cdd5bfabf0963313
urcm <76536007+urcm@users.noreply.github.com>
2021-11-25 23:45:55 +0100

    Update check_file_if_exists_in_folder.py

--- basic/check_file_if_exists_in_folder.py
+++ basic/check_file_if_exists_in_folder.py
@@ -1,1 +1,17 @@
 
+directory = 'F:'
+
+folderlistfull, folderlistfiles  = [], []
+for root, subdirectories, files in os.walk(directory):
+    for (a, subdirectory) in enumerate(subdirectories, start=1):
+        path = os.path.join(root, subdirectory)
+        folderlistfull.append(path)
+        list = os.listdir(path)
+        for filename in list:
+            file = os.path.join(path, filename)
+            if re.search('_icon', file):
+                folderlistfiles.append(path)
+            else:
+                pass
+
+folder_diff = set(folderlistfull).difference(set(folderlistfiles))
