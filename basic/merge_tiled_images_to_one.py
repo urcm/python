78592694f92c804ebed67fc28ba60de9ec12a1a6
urcm <76536007+urcm@users.noreply.github.com>
2021-11-29 23:29:41 +0100

    Update merge_tiled_images_to_one.py

--- basic/merge_tiled_images_to_one.py
+++ basic/merge_tiled_images_to_one.py
@@ -1,1 +1,15 @@
 
+for root, subdirectories, files in os.walk(directory):
+    for (a, subdirectory) in enumerate(subdirectories, start=1):
+        path = os.path.join(root, subdirectory)
+        list = os.listdir(path)
+        number_files = len(list)
+        arry = []
+        new_im = Image.new('RGB', (800, 800))
+        for filename in list:
+            file = os.path.join(path, filename)
+            if re.search('_icon', file):
+                pass
+            else:
+                arry.append(file)
+        images = []
