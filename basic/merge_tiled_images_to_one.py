6f35f2db38fb59e84ebae0f717e0db9b03fb6057
urcm <76536007+urcm@users.noreply.github.com>
2021-12-04 21:34:39 +0100

    Update merge_tiled_images_to_one.py

--- basic/merge_tiled_images_to_one.py
+++ basic/merge_tiled_images_to_one.py
@@ -22,3 +22,10 @@ for root, subdirectories, files in os.walk(directory):
         max_height = max(heights)
 
         new_im = Image.new('RGB', (total_width, max_height))
+        
+        x_offset = 0
+        for im in images:
+            new_im.paste(im, (x_offset, 0))
+            x_offset += im.size[0]
+
+    
